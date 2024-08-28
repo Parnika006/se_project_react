38156b4a79757e4fea98faea6f6fbed830fa32cd
Parnika006 <parnikasingh006@gmail.com>
2024-08-28 14:36:35 -0700

    project 11 esscape key update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -44,6 +44,23 @@ function App() {
     setActiveModal("");
   };
 
+  useEffect(() => {
+    if (!activeModal) return; // stop the effect not to add the listener if there is no active modal
+
+    const handleEscClose = (e) => {
+      if (e.key === "Escape") {
+        closeActiveModal();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscClose);
+
+    return () => {
+      // don't forget to add a clean up function for removing the listener
+      document.removeEventListener("keydown", handleEscClose);
+    };
+  }, [activeModal]);
+
   const handleToggleSwitchChange = () => {
     if (currentTemperatureUnit === "C") {
       setCurrentTemperatureUnit("F");
